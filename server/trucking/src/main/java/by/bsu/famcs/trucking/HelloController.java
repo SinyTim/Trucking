4c08b0cb17acb4418cd3561fa0bec74648e50bca
Timur <sinytim@mail.ru>
2020-05-06 17:14:56 +0300

    Add docker files

--- server/trucking/src/main/java/by/bsu/famcs/trucking/HelloController.java
+++ server/trucking/src/main/java/by/bsu/famcs/trucking/HelloController.java
@@ -1,11 +1,9 @@
 package by.bsu.famcs.trucking;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@CrossOrigin(origins={"https://trucking-famcs.herokuapp.com"})
 @RestController
 public class HelloController {
 
