854fed609ae07204bf69dc2f785936d0c30229c7
aliaksandra.kunitskaya <aliaksandra.kunitskaya@vizor-games.com>
2020-05-29 18:48:55 +0300

    Manager. Users page (CR + block/unblock)

--- client/trucking/src/app/_services/authentication.service.js
+++ client/trucking/src/app/_services/authentication.service.js
@@ -18,7 +18,7 @@ function login(username, password) {
         body: JSON.stringify({ username, password })
     };
 
-    return fetch(`api/users/authenticate`, requestOptions)
+    return fetch(`api/authenticate`, requestOptions)
         .then(handleResponse)
         .then(user => {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
--- client/trucking/src/app/admin/User.js
+++ client/trucking/src/app/admin/User.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {userService} from "../_services/user.service";
 
-let User = function statelessFunctionComponentClass(props) {
+export default class User extends React.Component {
 
-    let user = props.user;
+    constructor(props) {
+        super(props);
 
-    const cardStyle = {
+        this.state = {
+            reload: false,
+            user: props.user,
+            adminId: props.adminId
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    cardStyle = {
         width: '500px',
         height: '120px',
         margin: '30px 30px 30px 30px',
@@ -12,7 +24,7 @@ let User = function statelessFunctionComponentClass(props) {
         borderRadius: '5px',
     };
 
-    const cardHeaderStyle = {
+    cardHeaderStyle = {
         height: '20px',
         background: '#A4C3B2',
         borderRadius: '5px 5px 0 0',
@@ -21,7 +33,7 @@ let User = function statelessFunctionComponentClass(props) {
         padding: '20px'
     };
 
-    const cardDataSectionStyle = {
+    cardDataSectionStyle = {
         borderBottom: '2px solid #000000',
         marginLeft: '10%',
         paddingBottom: '10px',
@@ -30,24 +42,39 @@ let User = function statelessFunctionComponentClass(props) {
         display: 'flex',
     };
 
-    const cardDataStyle = {
-        padding: '10px 10px 0 20px',
+    cardDataStyle = {
+        padding: '15px 10px 0 20px',
         fontSize: '20px',
-        display: 'flex',
-
     };
 
-    return (
-        <div style={cardStyle}>
-            <div style={cardHeaderStyle}>
-                {user.firstName} {user.lastName}
-            </div>
+    changeUserBlockStatus() {
+        userService.changeBanStatus(this.state.adminId, this.state.user.id, !this.state.user.banned);
+        this.setState({user: {...this.state.user, banned: !this.state.user.banned}})
+    }
 
-            <div style={cardDataStyle}>
-               Role: {user.role}
-            </div>
-        </div>
-    );
-};
+    render() {
+        return (
+            <div style={this.cardStyle}>
+                <div style={this.cardHeaderStyle}>
+                    {this.state.user.firstName} {this.state.user.lastName}
 
-export default User;
+                    <div style={{float: 'right', marginTop: '-10px'}}>
+                        <button onClick={this.changeUserBlockStatus.bind(this)}>
+                            {this.state.user.banned ? 'unblock' : 'block'}
+                        </button>
+                    </div>
+                </div>
+
+                <div>
+                    <div style={Object.assign({}, this.cardDataStyle, {float: 'left',})}>
+                        Role: {this.state.user.role}
+                    </div>
+
+                    <div style={Object.assign({}, this.cardDataStyle, {float: 'right', color: '#F2766B'})}>
+                        {this.state.user.banned ? 'blocked' : 'not blocked'}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
