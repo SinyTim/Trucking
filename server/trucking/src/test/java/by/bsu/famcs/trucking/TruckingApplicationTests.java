c19b9d6a78df0e4900b4f6a9b1450248713b5912
Timur <sinytim@mail.ru>
2020-05-31 20:46:13 +0300

    Add tests

--- server/trucking/src/test/java/by/bsu/famcs/trucking/TruckingApplicationTests.java
+++ server/trucking/src/test/java/by/bsu/famcs/trucking/TruckingApplicationTests.java
@@ -1,13 +1,41 @@
 package by.bsu.famcs.trucking;
 
-import org.junit.jupiter.api.Test;
+import by.bsu.famcs.trucking.front.controller.HealthCheckController;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TruckingApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private HealthCheckController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
+	@Test
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc
+				.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("API works.")));
+	}
 }
